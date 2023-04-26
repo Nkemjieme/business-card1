f28e653591e6bbf667463517b018b8838a4e13ab
jollyguy123 <102703045+Nkemjieme@users.noreply.github.com>
2023-04-26 17:11:36 +0100

    add file via upload

---
+++ first.js
@@ -0,0 +1,38 @@
+// let firstCard = 10
+// let secondCard = 4
+// sum = firstCard + secondCard
+// let hasBlackJack = false
+// let isAlive = true
+// let message = ""
+// let messageEl = document.getElementById('message-el')
+// let sumEl = document.getElementById("sum-el")
+// let cardsEl = document.querySelector("#cards-el")
+// console.log(sumEl)
+// console.log(messageEl)
+//
+// function startGame(){
+//   renderGame()
+// }
+//
+// function renderGame(){
+//   if (sum <= 20){
+//       message = "Do you want to draw a new card?"
+//     } else if (sum === 21){
+//       message = "Wohoo You've got Blackjack!"
+//       hasBlackJack = true
+//     } else {
+//       message = "You lost!!"
+//       isAlive = false
+//     }
+//     messageEl.textContent = message
+//     sumEl.textContent = "sum: " + sum
+//     cardsEl.textContent = "cards: " + firstCard + " " + secondCard
+// }
+//
+// function newCard(){
+//   console.log("Drawing a new card from the deck")
+//   let card = 7
+//   sum += card
+//   renderGame()
+// }
+let myArray = ["my name is joel", 19, true]
